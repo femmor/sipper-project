6791e99467a47d597fefb6b4bfba7c40953ea20e
Emmanuel Egomson <fegomson@gmail.com>
2021-06-02 09:03:20 -0500

    About and Error Page Setup

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">About</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis beatae amet hic quo veritatis laborum delectus iure eius deserunt sed commodi temporibus totam officia pariatur, impedit tempore deleniti. Incidunt commodi, reprehenderit nostrum maiores esse consequatur a illo provident beatae non nulla ratione, saepe sint dolorem. Velit quia iusto nobis sapiente!</p>
+    </section>
   )
 }
 
